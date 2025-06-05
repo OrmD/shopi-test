09609d229926d5c6cb2bb01e5daaa99fc0d2e96f
Рома Кушнірчук <groma1535@gmail.com>
2025-06-05 23:18:21 +0300

    Loc | writing code for img in swaiper

--- assets/swaiper-test.js
+++ assets/swaiper-test.js
@@ -1,1 +1,25 @@
-console.log("okay")
+document.addEventListener("DOMContentLoaded", function () {
+	console.log('okay');
+	new Swiper(".mySwiper", {
+		slidesPerView: 1,
+		spaceBetween: 10,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		breakpoints: {
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+		}
+	});
+});
